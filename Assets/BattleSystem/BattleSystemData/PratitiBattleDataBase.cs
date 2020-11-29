826ccfb7a3aded3cc0bb1287fbf2a53c1d755894
Cobra30621 <cobra20621@gmail.com>
2020-11-29 17:32:58 +0800

    準備merge，先刪掉舊版戰鬥系統

--- Assets/BattleSystem/BattleSystemData/PratitiBattleDataBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "pratitiBattleDataBase", menuName = "CreatePratitiBattleDataBase")]
-public class PratitiBattleDataBase : ScriptableObject
-{
-    public List<PratitiBattleData> pratitiBattleDataList;
-}
